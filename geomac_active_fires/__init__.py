73640c75ff241ed26d6f5a051356147e53be1482
Casey Miller <caseymm@gmail.com>
2019-06-25 11:16:25 -0700

    use fiona to download and open zip file. artisanal geojson for now

--- geomac_active_fires/__init__.py
+++ geomac_active_fires/__init__.py
@@ -1,28 +1,25 @@
-import requests
-import zipfile
-# import tempfile
-import io
-import os
-import shapefile
-import json
+import fiona
+from geojson import Feature, FeatureCollection
 
 
 def get_geomac():
     """
     Get GeoMAC data
     """
-    tmp = io.BytesIO()
     active_fire_url = 'https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/current_year_fire_data/current_year_all_states/active_perimeters_dd83.zip'
-    r = requests.get(active_fire_url)
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    # dirpath = tempfile.mkdtemp()
-    # print(dirpath)
-    z.extractall('tmp')
-    shp = [ f for f in os.listdir('tmp') if f.endswith(".shp") ][0]
-    sf = shapefile.Reader('tmp/{}'.format(shp))
-    print(sf.__geo_interface__)
-    # with open('geomac.geojson', 'w') as outfile:
-    #     json.dump(sf.__geo_interface__, outfile)
+    with fiona.open('zip+{}'.format(active_fire_url)) as src:
+        features = []
+        for rec in src:
+            features.append(
+                    Feature(
+                        geometry=rec['geometry'],
+                        properties=rec
+                    )
+                )
+        collection = FeatureCollection(features)
+        with open("{}.geojson".format('geomac'), "w") as f:
+            f.write('%s' % collection)
+
 
 if __name__ == '__main__':
     get_geomac()
