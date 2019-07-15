c8627792292ad145852b2d54df67a8a7c6958f3a
Casey Miller <caseymm@gmail.com>
2019-07-15 16:31:21 -0700

    take out print statement

--- geomac_wildfires/__init__.py
+++ geomac_wildfires/__init__.py
@@ -16,5 +16,4 @@ def get_nifc_sitrep():
     """
     url = 'https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/current_year_fire_data/current_year_all_states/nifc_sit_rep_dd83.zip'
     with fiona.open(f'zip+{url}') as f:
-        print(FeatureCollection([Feature(geometry=d['geometry'], properties=d) for d in f]))
         return FeatureCollection([Feature(geometry=d['geometry'], properties=d) for d in f])
