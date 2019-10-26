5f48efcb091c680ba95b9ca838560855bfc2dbab
palewire <ben.welsh@gmail.com>
2019-10-26 13:17:46 -0700

    simplify

--- geomac_wildfires/__init__.py
+++ geomac_wildfires/__init__.py
@@ -1,7 +1,5 @@
 # from shapely.geometry import shape, mapping
 import fiona
-import requests
-from datetime import datetime, timezone
 from geojson import Feature, FeatureCollection
 
 
@@ -11,7 +9,9 @@ def get_active_fires():
     """
     url = 'https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/current_year_fire_data/current_year_all_states/active_perimeters_dd83.zip'
     with fiona.open(f'zip+{url}') as f:
-        return FeatureCollection([Feature(geometry=d['geometry'], properties=d['properties']) for d in f])
+        return FeatureCollection([
+            Feature(geometry=d['geometry'], properties=d['properties']) for d in f
+        ])
 
 
 def get_all_fires():
@@ -20,9 +20,9 @@ def get_all_fires():
     """
     url = 'https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/current_year_fire_data/current_year_all_states/perimeters_dd83.zip'
     with fiona.open(f'zip+{url}') as f:
-        return FeatureCollection(
-            [Feature(geometry=d['geometry'], properties=d['properties']) for d in f]
-        )
+        return FeatureCollection([
+            Feature(geometry=d['geometry'], properties=d['properties']) for d in f
+        ])
 
 
 def get_nifc_sitrep():
@@ -31,6 +31,6 @@ def get_nifc_sitrep():
     """
     url = 'https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/current_year_fire_data/current_year_all_states/nifc_sit_rep_dd83.zip'
     with fiona.open(f'zip+{url}') as f:
-        return FeatureCollection(
-            [Feature(geometry=d['geometry'], properties=d['properties']) for d in f]
-        )
+        return FeatureCollection([
+            Feature(geometry=d['geometry'], properties=d['properties']) for d in f
+        ])
